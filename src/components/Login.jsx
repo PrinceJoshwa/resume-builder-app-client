a985fb8fff0cc1550db8c7b8872757abce17afee
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-12 00:00:37 +0530

    ui changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useState } from 'react';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function Login({ setUser }) {
   const navigate = useNavigate();
